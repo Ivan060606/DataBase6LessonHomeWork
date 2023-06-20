e844c414080f6d2f4dc199946b4bc3a445581137
Ivan060606 <109799705+Ivan060606@users.noreply.github.com>
2023-06-20 20:54:35 +0300

    drop 3 task

--- 6lessdbhw.sql
+++ 6lessdbhw.sql
@@ -18,3 +18,58 @@ BEGIN
 END//
 
 SELECT NOW(), hello()//
+
+-- 3.(по желанию)* Создайте таблицу logs типа Archive. Пусть при каждом создании записи в таблицах users,
+-- communities и messages в таблицу logs помещается время и дата создания записи, название таблицы, идентификатор
+-- первичного ключа.
+
+DROP TABLE IF EXISTS logs;
+
+CREATE TABLE logs (
+  append_dt DATETIME DEFAULT CURRENT_TIMESTAMP,
+  append_tn VARCHAR (255),
+  pk_id INT UNSIGNED NOT NULL,
+  ) ENGINE ARCHIVE;
+
+DROP PROCEDURE IF EXISTS append_logs;
+DELIMITER //
+CREATE PROCEDURE append_logs (
+  dt DATETIME,
+  tn VARCHAR (255),
+  id INT
+)
+BEGIN
+	INSERT INTO logs (append_dt, append_tn, pk_id) VALUES (dt, tn, id);
+END //
+delimiter ;
+
+DROP TRIGGER IF EXISTS log_appending_from_users;
+delimiter //
+CREATE TRIGGER log_appending_from_users
+AFTER INSERT ON users
+FOR EACH ROW
+BEGIN
+	CALL append_logs(NEW.dt, 'users', NEW.id);
+END //
+delimiter ;
+
+DROP TRIGGER IF EXISTS log_appending_from_communities;
+delimiter //
+CREATE TRIGGER log_appending_from_communities
+AFTER INSERT ON communities
+FOR EACH ROW
+BEGIN
+	CALL append_logs(NEW.dt, 'communities', NEW.id);
+END //
+delimiter ;
+
+DROP TRIGGER IF EXISTS log_appending_from_messages;
+delimiter //
+CREATE TRIGGER log_appending_from_messages
+AFTER INSERT ON messages
+FOR EACH ROW
+BEGIN
+	CALL append_logs(NEW.dt, 'messages', NEW.id);
+END //
+delimiter ;
+  
