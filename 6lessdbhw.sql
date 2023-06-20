a275eba4597ed5df801449b1d2ba7a9ee629ca5c
Ivan060606 <109799705+Ivan060606@users.noreply.github.com>
2023-06-20 20:20:07 +0300

    drop 2 task

---
+++ 6lessdbhw.sql
@@ -0,0 +1,20 @@
+-- 2.Создайте хранимую функцию hello(), которая будет возвращать приветствие, в зависимости от текущего времени суток.
+-- С 6:00 до 12:00 функция должна возвращать фразу "Доброе утро", с 12:00 до 18:00 функция должна возвращать фразу
+-- "Добрый день", с 18:00 до 00:00 — "Добрый вечер", с 00:00 до 6:00 — "Доброй ночи".
+
+DELIMITER //
+
+CREATE FUNCTION hello ()
+RETURNS TINYTEXT NOT DETERMINISTIC
+BEGIN
+	DECLARE hour INT;
+	SET hour = HOUR(NOW());
+	CASE
+		WHEN hour BETWEEN 0 AND 5 THEN RETURN "Доброй ночи";
+		WHEN hour BETWEEN 6 AND 11 THEN RETURN "Доброе утро";
+		WHEN hour BETWEEN 12 AND 17 THEN RETURN "Добрый день";
+		WHEN hour BETWEEN 18 AND 23 THEN RETURN "Добрый вечер";
+	END CASE;
+END//
+
+SELECT NOW(), hello()//
