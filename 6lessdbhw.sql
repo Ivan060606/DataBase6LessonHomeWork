d4318b6cadbd98c630ebe70c060c1661512470f2
Ivan060606 <109799705+Ivan060606@users.noreply.github.com>
2023-06-21 02:18:45 +0300

    Update 1task 6lessdbhw.sql

--- 6lessdbhw.sql
+++ 6lessdbhw.sql
@@ -1,3 +1,37 @@
+-- 1.Создайте таблицу users_old, аналогичную таблице users. Создайте процедуру, с помощью которой можно переместить
+-- любого (одного) пользователя из таблицы users в таблицу users_old. (использование транзакции с выбором commit или
+-- rollback – обязательно).
+
+USE 4lessdbhw;
+
+DROP TABLE IF EXISTS users_old;
+CREATE TABLE users_old (
+	id SERIAL PRIMARY KEY, -- SERIAL = BIGINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE
+    firstname VARCHAR(50),
+    lastname VARCHAR(50) COMMENT 'Фамилия',
+    email VARCHAR(120) UNIQUE
+);
+
+SELECT * FROM users_old;
+
+DROP PROCEDURE IF EXISTS move_it;
+DELIMITER //
+CREATE PROCEDURE move_it (
+	idt INT
+)
+BEGIN
+	START TRANSACTION;
+	SELECT * FROM users
+    WHERE id = idt;
+	INSERT INTO users_old (SELECT * FROM users WHERE id = idt);
+    DELETE FROM `users` WHERE `id` = idt LIMIT 1;
+    	
+	COMMIT;
+END //
+DELIMITER ;
+
+call move_it(3);
+
 -- 2.Создайте хранимую функцию hello(), которая будет возвращать приветствие, в зависимости от текущего времени суток.
 -- С 6:00 до 12:00 функция должна возвращать фразу "Доброе утро", с 12:00 до 18:00 функция должна возвращать фразу
 -- "Добрый день", с 18:00 до 00:00 — "Добрый вечер", с 00:00 до 6:00 — "Доброй ночи".
